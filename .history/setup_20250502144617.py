13e57749cbaccdcfd093982cdffe76b06f4b2cbf
vishan <vishanshingala@gmail.com>
2025-05-02 15:38:06 +0530

    project structure set

---
+++ .history/setup_20250502144617.py
@@ -0,0 +1,3 @@
+  File "<stdin>", line 1, in <module>
+NameError: name 'list_of_files' is not defined
+>>> 
---
+++ .history/setup_20250502144658.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Vishan Shingala",
+    author_email="vishanhshingala@gmail.com",
+    packages=find_packages()
+)
